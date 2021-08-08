bca2e10e704fa276f927ae0a3804e14f943066c3
skylerlee <skyler.ac.lee@gmail.com>
2021-08-09 00:56:14 +0800

    feat: test code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include "cactus.h"
+
 #include <stdio.h>
 
+#include "parser/grammar.h"
+
 int main(int argc, char* argv[]) {
-  printf("It works!\n");
+  cactus_context_t* ctx = cactus_create(NULL);
+  cactus_parse(ctx, NULL);
+  cactus_destroy(ctx);
   return 0;
 }
