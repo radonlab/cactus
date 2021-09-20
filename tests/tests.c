740615547370cadb59a69649b9481c1956c60c63
skylerlee <skyler.ac.lee@gmail.com>
2021-09-20 17:15:53 +0800

    feat: add tests

---
+++ tests/tests.c
@@ -0,0 +1,5 @@
+#include "tests.h"
+
+int main(int argc, char *argv[]) {
+  return clar_test(argc, argv);
+}
---
+++ tests/tests.h
@@ -0,0 +1,6 @@
+#ifndef TESTS_TESTS_H_
+#define TESTS_TESTS_H_
+
+#include "clar.h"
+
+#endif  // TESTS_TESTS_H_
