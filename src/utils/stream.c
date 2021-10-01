8fbb08646256d43f780daf6f0af414bd5e07fa70
skylerlee <skyler.ac.lee@gmail.com>
2021-10-01 19:58:19 +0800

    feat: update

--- src/utils/stream.c
+++ src/utils/stream.c
@@ -4,12 +4,20 @@ size_t cactus_istream_read(cactus_istream_t* stream, uint8_t buf[], size_t len) 
   return stream->ops->read(stream, buf, len);
 }
 
+bool cactus_istream_seek(cactus_istream_t* stream, size_t pos) {
+  return stream->ops->seek(stream, pos);
+}
+
 bool cactus_istream_close(cactus_istream_t* stream) {
   return stream->ops->close(stream);
 }
 
 size_t cactus_ostream_write(cactus_ostream_t* stream, const uint8_t* buf, size_t len) {
   return stream->ops->write(stream, buf, len);
+}
+
+bool cactus_ostream_flush(cactus_ostream_t* stream) {
+  return stream->ops->flush(stream);
 }
 
 bool cactus_ostream_close(cactus_ostream_t* stream) {
--- src/utils/stream.h
+++ src/utils/stream.h
@@ -22,25 +22,35 @@ typedef struct cactus_ostream_t {
   const cactus_ostream_ops_t* ops;
 } cactus_ostream_t;
 
+/**
+ * Input stream operations
+ */
 struct cactus_istream_ops_t {
   size_t (*read)(cactus_istream_t* stream, uint8_t* buf, size_t len);
+  bool (*seek)(cactus_istream_t* stream, size_t pos);
   bool (*close)(cactus_istream_t* stream);
 };
 
+/**
+ * Output stream operations
+ */
 struct cactus_ostream_ops_t {
   size_t (*write)(cactus_ostream_t* stream, const uint8_t* buf, size_t len);
+  bool (*flush)(cactus_ostream_t* stream);
   bool (*close)(cactus_ostream_t* stream);
 };
 
+// Input stream methods
+
 size_t cactus_istream_read(cactus_istream_t* stream, uint8_t* buf, size_t len);
+bool cactus_istream_seek(cactus_istream_t* stream, size_t pos);
 bool cactus_istream_close(cactus_istream_t* stream);
+
+// Output stream methods
 
 size_t cactus_ostream_write(cactus_ostream_t* stream, const uint8_t* buf,
                             size_t len);
+bool cactus_ostream_flush(cactus_ostream_t* stream);
 bool cactus_ostream_close(cactus_ostream_t* stream);
-
-// size_t cactus_ostream_write(cactus_stream_t* stream, uint8_t* buf, size_t
-// len); void cactus_ostream_flush(cactus_stream_t* stream); bool
-// cactus_ostream_close(cactus_stream_t* stream);
 
 #endif  // CACTUS_UTILS_STREAM_H_
