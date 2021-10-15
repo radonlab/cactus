a72aa11a57eaff654df7f03eaa2d59cd6d3555ac
skylerlee <skyler.ac.lee@gmail.com>
2021-10-16 01:36:22 +0800

    feat: update

--- src/base/buf_stream.c
+++ src/base/buf_stream.c
@@ -1,5 +1,7 @@
 #include "buf_stream.h"
 
+#define ENLARGE_CAP(x) (x + x >> 1)
+
 static size_t buf_istream_read(cactus_buf_istream_t* stream, uint8_t* buf, size_t len) { return len; }
 
 static bool buf_istream_seek(cactus_buf_istream_t* stream, size_t pos) { return true; }
@@ -22,6 +24,6 @@ cactus_istream_t* cactus_buf_istream_init(cactus_buf_istream_t* stream, const ui
   stream->base.ops = &buf_istream_ops;
 }
 
-cactus_ostream_t* cactus_buf_ostream_init(cactus_buf_ostream_t* stream, size_t size) {
+cactus_ostream_t* cactus_buf_ostream_init(cactus_buf_ostream_t* stream) {
   stream->base.ops = &buf_ostream_ops;
 }
--- src/base/buf_stream.h
+++ src/base/buf_stream.h
@@ -3,11 +3,16 @@
 
 #include "stream.h"
 
+#define BUF_STREAM_INIT_CAP 0x400
+
 /**
  * Buffer input stream
  */
 typedef struct cactus_buf_istream_t {
   cactus_istream_t base;
+  uint8_t* buf;
+  size_t size;
+  size_t offset;
 } cactus_buf_istream_t;
 
 /**
@@ -15,10 +20,13 @@ typedef struct cactus_buf_istream_t {
  */
 typedef struct cactus_buf_ostream_t {
   cactus_ostream_t base;
+  uint8_t* buf;
+  size_t size;
+  size_t capacity;
 } cactus_buf_ostream_t;
 
 cactus_istream_t* cactus_buf_istream_init(cactus_buf_istream_t* stream, const uint8_t* buf, size_t size);
 
-cactus_ostream_t* cactus_buf_ostream_init(cactus_buf_ostream_t* stream, size_t size);
+cactus_ostream_t* cactus_buf_ostream_init(cactus_buf_ostream_t* stream);
 
 #endif  // CACTUS_BASE_BUF_STREAM_H_
