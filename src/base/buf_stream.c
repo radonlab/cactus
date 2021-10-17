343c4e9b9bfb6344a6909cb75efc69869c2d5dac
skylerlee <skyler.ac.lee@gmail.com>
2021-10-17 17:50:45 +0800

    feat: update buffer stream

--- src/base/buf_stream.c
+++ src/base/buf_stream.c
@@ -1,14 +1,65 @@
 #include "base/buf_stream.h"
 
+#include <stdio.h>
+#include <string.h>
+
 #include "base/common.h"
 
-#define ENLARGE_CAP(x) (x + x >> 1)
+#define ENLARGE_CAPACITY(x) (x + x >> 1)
 
-static size_t buf_istream_read(cac_buf_istream_t* stream, uint8_t* buf, size_t len) { return len; }
+static size_t buf_istream_read(cac_buf_istream_t* stream, uint8_t* buf, size_t len) {
+  if (len == 0) {
+    return 0;
+  }
+  if (stream->offset >= stream->size) {
+    // underflowed
+    return 0;
+  }
+  // copy left n bytes at most
+  size_t count = stream->size - stream->offset;
+  if (len < count) {
+    count = len;
+  }
+  memcpy(buf, stream->buf + stream->offset, count);
+  // forward offset
+  stream->offset += count;
+  return count;
+}
 
-static bool buf_istream_seek(cac_buf_istream_t* stream, size_t pos, int whence) {
+static bool buf_istream_seek(cac_buf_istream_t* stream, size_t off, int whence) {
+  if (whence == SEEK_SET) {
+    stream->offset = off;
+  } else if (whence == SEEK_CUR) {
+    stream->offset += off;
+  } else if (whence == SEEK_END) {
+    stream->offset = stream->size + off;
+  } else {
+    // invalid whence
+    return false;
+  }
+  return true;
+}
 
-static size_t buf_ostream_write(cac_buf_ostream_t* stream, const uint8_t* buf, size_t len) { return len; }
+static bool buf_istream_close(cac_buf_istream_t* stream) {
+  return true;
+}
+
+static void buf_ostream_ensure_capacity(cac_buf_ostream_t* stream, size_t size) {
+  if (size > stream->capacity) {
+    size_t new_capacity = ENLARGE_CAPACITY(stream->capacity);
+    stream->buf = cac_realloc(stream->buf, new_capacity);
+    stream->capacity = new_capacity;
+  }
+}
+
+static size_t buf_ostream_write(cac_buf_ostream_t* stream, const uint8_t* buf, size_t len) {
+  size_t newsize = stream->size + len;
+  buf_ostream_ensure_capacity(stream, newsize);
+  memcpy(stream->buf + stream->size, buf, len);
+  // accumulate size
+  stream->size += len;
+  return len;
+}
 
 static bool buf_ostream_flush(cac_buf_ostream_t* stream) {
   return true;
