91274a7ddaf515e85487c7965c25fc92f67c9ab0
skylerlee <skyler.ac.lee@gmail.com>
2021-11-01 01:08:53 +0800

    fix: fix cleanup

--- tests/base/buf_stream.c
+++ tests/base/buf_stream.c
@@ -4,12 +4,23 @@
 
 #include "tests.h"
 
+static void cleanup_istream(void* opaque) {
+  cac_istream_t* stream = opaque;
+  cac_istream_close(stream);
+}
+
+static void cleanup_ostream(void* opaque) {
+  cac_ostream_t* stream = opaque;
+  cac_ostream_close(stream);
+}
+
 void test_base__buf_stream__read() {
   char* str = "foobarbaz";
   char buf[5];
   size_t n;
-  cac_buf_istream_t buf_stream;
+  static cac_buf_istream_t buf_stream;
   cac_istream_t* stream = cac_buf_istream_init(&buf_stream, (uint8_t*)str, strlen(str));
+  cl_set_cleanup(&cleanup_istream, stream);
   n = cac_istream_read(stream, (uint8_t*)buf, sizeof(buf));
   cl_assert_equal_i(n, 5);
   cl_assert_equal_strn(buf, "fooba", n);
@@ -27,8 +38,9 @@ void test_base__buf_stream__write() {
   char str3[2048];
   size_t total = 0;
   size_t n;
-  cac_buf_ostream_t buf_stream;
+  static cac_buf_ostream_t buf_stream;
   cac_ostream_t* stream = cac_buf_ostream_init(&buf_stream);
+  cl_set_cleanup(&cleanup_ostream, stream);
   n = cac_ostream_write(stream, (uint8_t*)str1, strlen(str1));
   total += n;
   cl_assert_equal_i(n, 3);
