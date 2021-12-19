10ac46c666f65b187c0fbc65497de47dd34ef199
skylerlee <skyler.ac.lee@gmail.com>
2021-12-19 22:51:00 +0800

    feat: update error reporting

--- tests/tests.c
+++ tests/tests.c
@@ -5,10 +5,15 @@
 
 #include "cwalk.h"
 
+static char* test_data_dir_var = "CAC_TEST_DATA_DIR";
 static char path_str_buf[FILENAME_MAX];
 
 char* path_of_test_data(char* rel_path) {
-  char* test_data_dir = getenv("CACTUS_TEST_DATA_DIR");
+  char* test_data_dir = getenv(test_data_dir_var);
+  if (test_data_dir == NULL) {
+    fprintf(stderr, "error: environment variable '%s' not defined\n", test_data_dir_var);
+    exit(1);
+  }
   cwk_path_join(test_data_dir, rel_path, path_str_buf, sizeof(path_str_buf));
   return path_str_buf;
 }
